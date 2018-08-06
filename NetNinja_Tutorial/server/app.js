74ce593386b52e801bcca52c5df67180bfd448f3
ChristopherALee <christopheradamlee@gmail.com>
2018-08-06 17:32:04 -0400

    created root query

--- NetNinja_Tutorial/server/app.js
+++ NetNinja_Tutorial/server/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // /graphql path in url request
 // object in graphqlHTTP is the schema
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema
+  })
+);
 
 app.listen(3000, () => {
   console.log("now listening for requests on port 3000");
