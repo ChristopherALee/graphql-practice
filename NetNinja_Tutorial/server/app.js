ea5edb5bd88dc2b728cb978595b0e1800b36fe87
ChristopherALee <christopheradamlee@gmail.com>
2018-08-06 15:17:13 -0400

    setup graphql in node

--- NetNinja_Tutorial/server/app.js
+++ NetNinja_Tutorial/server/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
 const app = express();
+
+// /graphql path in url request
+// object in graphqlHTTP is the schema
+app.use("/graphql", graphqlHTTP({}));
 
 app.listen(3000, () => {
   console.log("now listening for requests on port 3000");
