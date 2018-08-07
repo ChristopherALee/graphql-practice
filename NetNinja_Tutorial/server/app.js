ae73d8a30e15b272bc5a420689e1ebc62167a908
ChristopherALee <christopheradamlee@gmail.com>
2018-08-07 14:54:44 -0400

    installed mongoose ORM and setup connection to mongoLab (mLab)

--- NetNinja_Tutorial/server/app.js
+++ NetNinja_Tutorial/server/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect(
+  "mongodb://creme:test123@ds215502.mlab.com:15502/graphql-test",
+  {
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 // /graphql path in url request
 // object in graphqlHTTP is the schema
