7a3c9722c87ffaa16f5282f07cad6895fb65bd60
ChristopherALee <christopheradamlee@gmail.com>
2018-08-09 16:55:39 -0400

    created form for submitting a book w/ handleChange and handleSubmit methods and local state

--- NetNinja_Tutorial/client/src/components/AddBook.jsx
+++ NetNinja_Tutorial/client/src/components/AddBook.jsx
@@ -6,6 +6,26 @@ import { getAuthorsQuery } from "../queries/queries";
 class AddBook extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      bookName: "",
+      genre: "",
+      author: "5b6a666c1cc787bbaf6fddf0"
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(field) {
+    return e => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
   }
 
   displayAuthors() {
@@ -26,20 +46,22 @@ class AddBook extends React.Component {
 
   render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.handleSubmit}>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input type="text" onChange={this.handleChange("bookName")} />
         </div>
 
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input type="text" onChange={this.handleChange("genre")} />
         </div>
 
         <div className="field">
           <label>Author:</label>
-          <select>{this.displayAuthors()}</select>
+          <select onChange={this.handleChange("author")}>
+            {this.displayAuthors()}
+          </select>
         </div>
 
         <button>+</button>
