48da1968e4cf6aca09287a3be5fcf9b19375eb2b
ChristopherALee <christopheradamlee@gmail.com>
2018-09-18 14:28:02 -0400

    created book model, graphql schemas (BookType and RootQuery), and setup mlab db

--- the_collective/server/app.js
+++ the_collective/server/app.js
@@ -5,3 +5,28 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+// mlab connection
+mongoose.connect(
+  "mongodb://creme:fraiche123@ds261342.mlab.com:61342/the_collective",
+  {
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to mlab database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => {
+  console.log("now listening for requests on port 3000");
+});
--- the_collective/server/schema/schema.js
+++ the_collective/server/schema/schema.js
@@ -1,2 +1,40 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+
+const Book = require("../models/book");
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull
+} = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find({});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
