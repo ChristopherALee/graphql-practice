f1ec189e3299d73cd6e10bab078aef4632389e92
ChristopherALee <christopheradamlee@gmail.com>
2018-10-02 10:45:01 -0400

    migrated finish editing styling to books category

--- the_collective/src/components/collections/books/editBooks/editBook.jsx
+++ the_collective/src/components/collections/books/editBooks/editBook.jsx
@@ -13,25 +13,39 @@ class EditBook extends React.Component {
     super(props);
 
     this.state = {
+      id: this.props.book.id,
       name: this.props.book.name,
       author: this.props.book.author,
-      genre: this.props.book.genre
+      genre: this.props.book.genre,
+      isFinishButtonActive: false
     };
 
+    this.toggleActive = this.toggleActive.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.editBook = this.editBook.bind(this);
   }
 
+  toggleActive() {
+    if (this.state.isFinishButtonActive) {
+      return "active-edit";
+    } else {
+      return "inactive-edit";
+    }
+  }
+
   handleChange(field) {
     return e => {
-      this.setState({ [field]: e.target.value });
+      this.setState({
+        [field]: e.target.value,
+        isFinishButtonActive: true
+      });
     };
   }
 
   editBook() {
     this.props.editBookMutation({
       variables: {
-        id: this.props.book.id,
+        id: this.state.id,
         name: this.state.name,
         author: this.state.author,
         genre: this.state.genre
@@ -90,7 +104,11 @@ class EditBook extends React.Component {
           />
         </td>
 
-        <td id="done-editing" onClick={this.editBook}>
+        <td
+          id="done-editing"
+          className={this.toggleActive()}
+          onClick={this.editBook}
+        >
           Finish Editing
         </td>
       </tr>
